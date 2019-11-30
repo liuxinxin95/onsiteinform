e0cc2121b925f5225c785d9e384e19fa3be7fe98
lxx <18634318474@163.com>
2019-11-30 23:49:50 +0800

    项目搭建

---
+++ src/main/java/com/cpy/onsiteinform/center/user/mapper/MngSysUserDOMapper.java
@@ -0,0 +1,7 @@
+package com.cpy.onsiteinform.center.user.mapper;
+
+import com.cpy.onsiteinform.center.user.entity.MngSysUserDO;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface MngSysUserDOMapper extends Mapper<MngSysUserDO> {
+}
