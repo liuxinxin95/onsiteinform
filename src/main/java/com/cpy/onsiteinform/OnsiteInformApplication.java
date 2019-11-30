6a679fb8e9664f79d7a294f73723c6c8b94137fc
lxx <18634318474@163.com>
2019-11-30 22:53:37 +0800

    11

---
+++ src/main/java/com/cpy/onsiteinform/OnsiteInformApplication.java
@@ -0,0 +1,13 @@
+package com.cpy.onsiteinform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnsiteInformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnsiteInformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cpy/onsiteinform/OnsiteInformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpy.onsiteinform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnsiteInformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
