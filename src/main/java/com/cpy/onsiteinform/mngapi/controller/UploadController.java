2e0ada4daedde85beb0dea84017a4df2e85eaca1
lxx <18634318474@163.com>
2019-12-01 22:43:19 +0800

    上传图片

---
+++ src/main/java/com/cpy/onsiteinform/mngapi/controller/UploadController.java
@@ -0,0 +1,34 @@
+package com.cpy.onsiteinform.mngapi.controller;
+
+import com.cpy.onsiteinform.framework.ApiResponse;
+import com.cpy.onsiteinform.framework.BaseController;
+import com.cpy.onsiteinform.util.UploadUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author Carl
+ * @ClassName UploadController
+ * @Description
+ * @date 2019-11-13 21:58
+ **/
+@Api("上传")
+@RestController
+@RequestMapping("mng/api/upload")
+public class UploadController extends BaseController {
+
+    /**
+     * 上传
+     */
+    @ApiOperation("上传")
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public ApiResponse<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        String upload = UploadUtil.upload(file);
+        return success(upload);
+    }
+}
--- src/main/java/com/cpy/onsiteinform/util/UploadUtil.java
+++ src/main/java/com/cpy/onsiteinform/util/UploadUtil.java
@@ -21,6 +21,7 @@ import java.io.IOException;
  **/
 public class UploadUtil {
 
+    private static String url = "https://onsiteimage.wangdamei.com/";
 
     public static String upload(MultipartFile file) {
         File localFile = null;
@@ -31,17 +32,17 @@ public class UploadUtil {
             e.printStackTrace();
         }
         // 1 初始化用户身份信息（secretId, secretKey）。
-        String secretId = "AKIDoU1RjpR1wjOtOok8d0EKH5ku32tb7g2G";
-        String secretKey = "vY1KWYTFrv460qZ9bvQ0SlssreINV4Vs";
+        String secretId = "AKIDKURAfxbqUu52mI5fVBdrrdATYQ4XHnY3";
+        String secretKey = "LQEl47uQimcEcleGcgU1HGyPZT5RWKrB";
         COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
         // 2 设置 bucket 的区域, COS 地域的简称请参照 https://cloud.tencent.com/document/product/436/6224
         // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
-        Region region = new Region("ap-beijing");
+        Region region = new Region("ap-guangzhou");
         ClientConfig clientConfig = new ClientConfig(region);
         // 3 生成 cos 客户端。
         COSClient cosClient = new COSClient(cred, clientConfig);
         //存储桶名称，格式：BucketName-APPID
-        String bucketName = "51html-1300626312";
+        String bucketName = "onsiteinform-1252452432";
         String key = "image/" + System.currentTimeMillis() + ".png";
         // 简单文件上传, 最大支持 5 GB, 适用于小文件上传, 建议 20 M 以下的文件使用该接口
         // 大文件上传请参照 API 文档高级 API 上传
@@ -49,10 +50,8 @@ public class UploadUtil {
         PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, localFile);
         PutObjectResult putObjectResult = cosClient.putObject(putObjectRequest);
         String etag = putObjectResult.getETag();
-        String rtValue = "https://51html-1300626312.cos.ap-beijing.myqcloud.com/" + key;
+        String rtValue = url + key;
         cosClient.shutdown();
-//        Date expiration = new Date(System.currentTimeMillis() + 5 * 60 * 10000);
-//        URL url = cosClient.generatePresignedUrl(bucketName, key, expiration);
         return rtValue;
     }
 
